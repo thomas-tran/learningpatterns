a67a139285982c19e1960324cc9156abaa1b6d89
Thomas Tran <tldinteractive@gmail.com>
2015-08-20 16:14:18 +1000

    singleton pattern thread safe without using lock

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace SingletonPattern
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Earth e1 = Earth.Instance;
+            Earth e2 = Earth.Instance;
+
+            if (e1 == e2)
+                Console.WriteLine("Only one earth available");
+
+            Console.ReadLine();
+        }
+
+
+    }
+
+    /// <summary>
+    /// Thread safe without using locks - not quite lazy
+    /// </summary>
+    public sealed class Earth
+    {
+        private static readonly Earth instance = new Earth();
+
+        static Earth() { }
+
+        private Earth() { }
+
+        public static Earth Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
---
+++ SingletonPattern/README.md
@@ -0,0 +1,11 @@
+Singleton Pattern
+===============
+Singleton pattern is used when you want to eliminate the option of instantiating
+more than one object. 
+
+Singleton don't allow any parameters to be specified when creating the instance.
+
+
+
+
+
